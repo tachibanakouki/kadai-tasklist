172cfa8d71d3632daccaee78498ca8958e96c52c
EC2 Default User <ec2-user@ip-172-31-62-212.ec2.internal>
2018-10-13 14:18:21 +0000

    kadai-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,6 +14,7 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.new(task_params)
+    
     if @task.save
       flash[:success] = "タスクを登録しました。"
       redirect_to task_url(@task)
@@ -23,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -39,7 +39,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-        @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -48,7 +47,12 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+    
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
